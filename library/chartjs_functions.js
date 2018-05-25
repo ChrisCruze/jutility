ff19271dfbc236e05f9bcf74473a54175b9ed700
ChrisCruze <cruzc09@gmail.com>
2018-05-25 18:05:47 -0400

    added chart js functions

---
+++ library/chartjs_functions.js
@@ -0,0 +1,21 @@
+
+//initiates a simple bar chart using chartjs
+function bar_chart_initiate_render_chartjs(chart_id,labels,numbers_list,colors){
+  labels = labels||['No Data']
+  numbers_list = numbers_list||[0]
+  colors = colors||["#a3e1d4"]
+
+  simple_chart_data = {labels:labels, datasets: [{data: numbers_list, backgroundColor: colors }] };
+  simple_options = {legend: {display: false}, responsive: true, tooltips: {enabled: true}};
+
+  var ctx = document.getElementById(chart_id).getContext("2d");
+  simple_chart_object = new Chart(ctx, {type: 'bar', data: simple_chart_data, options:simple_options});
+  return simple_chart_object
+}
+
+//updates bar_chart for data 
+function bar_chart_update_chartjs(chart_object,new_labels,new_data_points,new_colors){
+    chart_object.data.labels = new_labels // ['label a','label b']
+    chart_object.data.datasets[0].data = new_data_points//[1,2]
+    chart_object.data.datasets[0].backgroundColor = new_colors//["#a3e1d4","#dedede"]
+}
