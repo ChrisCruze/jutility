aff662179f13df1c28c5a1e979fd862b429a6cce
ChrisCruze <cruzc09@gmail.com>
2018-06-01 14:47:04 -0400

    added jutility html

--- function/file_functions.js
+++ function/file_functions.js
@@ -15,6 +15,11 @@ function read_text_file_data(file){
   rawFile.send(null);
 };
 
+//read directly from a text file
+function read_text_file_data_pull(file){
+  read_text_file_data(file)
+  return raw_text_file_data
+};
 //convert a downloadable data url into a download
 function downloadURI(uri, name) {
   var link = document.createElement("a");
@@ -24,4 +29,24 @@ function downloadURI(uri, name) {
   link.click();
   document.body.removeChild(link);
   delete link;
+}
+
+//use paparse to read from file
+function papa_parse_array(file,delimter){
+    delimter = delimter || "|"
+    file = file ||""
+    read_text_file_data(file)
+    var results = Papa.parse(raw_text_file_data);
+    text_lines = results.data
+    key_names = text_lines[0] 
+    key_names = headers_key_names_list_format_string(key_names)
+    array = []
+    text_lines.forEach(function(entry,i){
+        var singleObj = {}
+         key_names.forEach(function(kn,num_index){
+            singleObj[kn] = entry[num_index]
+         })
+        array.push(singleObj)
+    })
+    return array 
 }
