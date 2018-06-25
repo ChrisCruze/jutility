f4fb4f12e791fd7abb4589673cec9d24c636cf15
ChrisCruze <cruzc09@gmail.com>
2018-06-25 00:48:04 -0400

    update for fast timer

--- jutility.js
+++ jutility.js
@@ -565,7 +565,35 @@ function bar_chart_initiate_render_chartjs(chart_id,labels,numbers_list,colors){
   colors = colors||["#a3e1d4"]
 
   simple_chart_data = {labels:labels, datasets: [{data: numbers_list, backgroundColor: colors }] };
-  simple_options = {legend: {display: false},        scales: {
+  
+
+
+
+  simple_options = {    events: false,
+    tooltips: {
+        enabled: false
+    },
+    hover: {
+        animationDuration: 0
+    },
+    animation: {
+        duration: 1,
+        onComplete: function () {
+            var chartInstance = this.chart,
+                ctx = chartInstance.ctx;
+            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'bottom';
+
+            this.data.datasets.forEach(function (dataset, i) {
+                var meta = chartInstance.controller.getDatasetMeta(i);
+                meta.data.forEach(function (bar, index) {
+                    var data = dataset.data[index];                            
+                    ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                });
+            });
+        }
+    },legend: {display: false},        scales: {
           yAxes: [{
             stacked: true,
             ticks: {
