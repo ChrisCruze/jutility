38e140a08ebe72d5bd6ed7b3ed5bb73a8444fc8c
ChrisCruze <cruzc09@gmail.com>
2018-09-22 19:23:33 -0400

    upload for the latest

--- api/asana_functions.js
+++ api/asana_functions.js
@@ -80,13 +80,58 @@ function asana_tasks_get(project_id){
 	//
 }
 
+
+
+function asana_task_get(task_id){
+	asana_token = "110000 101111 110111 111000 110001 1100010 110110 1100001 111001 1100100 1100001 1100011 1100001 110110 110010 110001 1100110 110000 110100 1100110 1100110 110010 111001 1100101 110100 1100011 1100110 110100 1100011 110111 110100 110001 1100001 1100101"
+	result = $.ajax({
+	      type: "GET",
+	      url: "https://app.asana.com/api/1.0/tasks/"+task_id,
+	      dataType: 'json',
+	      async: false,
+	      data: {
+	        'access_token': binary_to_string(asana_token),
+	      }
+	    });
+
+	return result.responseJSON['data']
+}
 //800362391724583
 
 function asana_tasks_get_from_projects(){
 	projects_array = asana_projects_get()
-	project_ids = _.map(projects_array,function(D){return asana_tasks_get(D['id'])})
-	return project_ids
+	full_array = []
+	project_ids = _.map(projects_array,function(D){
+		tasks_group = asana_tasks_get(D['id'])
+		full_array = full_array.concat(tasks_group)
+		return tasks_group
+
+	})
+	return full_array
 }
+
+function asana_tasks_detail_pull(){
+	full_array = asana_tasks_get_from_projects()
+	task_details = _.map(full_array,function(D){return asana_task_get(D['id'])})
+	return task_details
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //GET    /projects/project-id/tasks
 //GET    /projects/project-id/tasks
